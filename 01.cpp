894395211aa1ca7871c37d851cd34c46a29b46a1
bunbogioheo <nhathan.creator@gmail.com>
2022-01-05 14:25:19 +0700

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool gapfulNumber(int n)
+{
+    string s;
+    s = to_string(n);
+    if (strlen(s)==3)
+    {
+        int a = n/100
+    }
+    
+
+}
+int main(){
+
+}
